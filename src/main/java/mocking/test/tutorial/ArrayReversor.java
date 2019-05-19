ec81960d414067651c359489c99144a74ae1205c
Nguezang Nsaha <nsahaarsene@yahoo.com>
2019-05-18 22:22:50 -0500

    Mocking Tutorial

---
+++ src/main/java/mocking/test/tutorial/ArrayReversor.java
@@ -0,0 +1,28 @@
+package mocking.test.tutorial;
+
+import mocking.test.tutorial.service.ArrayFlattenerService;
+
+public class ArrayReversor {
+    private ArrayFlattenerService arrayFlattenerService;
+
+    public ArrayReversor(ArrayFlattenerService arrayFlattenerService) {
+        this.arrayFlattenerService = arrayFlattenerService;
+    }
+
+
+    public int[] reversedArray(int [][] a_in){
+        int[] a_flat = arrayFlattenerService.flattenArray(a_in);
+        if(a_flat == null)
+            return null;
+        int i = 0;
+        int j = a_flat.length - 1;
+        while(i < j) {
+            int temp = a_flat[i];
+            a_flat[i] = a_flat[j];
+            a_flat[j] = temp;
+            i++;
+            j--;
+        }
+        return a_flat;
+    }
+}
---
+++ src/main/java/mocking/test/tutorial/service/ArrayFlattenerService.java
@@ -0,0 +1,5 @@
+package mocking.test.tutorial.service;
+
+public interface ArrayFlattenerService {
+    public int[] flattenArray(int[][] a_in);
+}
